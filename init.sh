65532fe8a75c9446fea2d576820ecb8b53fa80d3
Wei Gao <gw8310@gmail.com>
2016-04-13 20:05:42 +1000

    git init config

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@ cp -R ./.vim ~/
 cp ./.vimrc ~/
 chmod 777 ~/.vim
 chmod 777 ~/.vimrc
+
+git config --global user.name "Wei Gao"
+git config --global user.email "gw8310@gmail.com"
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
