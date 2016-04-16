8632fb19a37f8efb983c9ca4d55185d3be7aad2e
Wei Gao <gw8310@gmail.com>
2016-04-16 19:33:28 +1000

    update init.sh

--- init.sh
+++ init.sh
@@ -11,3 +11,13 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global push.default simple
+git config --global pull.rebase true
+git config --global rerere.enabled true
+git config --global alias.ss "status –s"
+git config --global alias.lg "log –oneline –decorate --all --graph"
+git config ­­--global alias.mylog "log ­­pretty=format:'%h %s [%an]' –graph"
+git config --global core.editor "mvim -f"
+git config --­­global merge.tool vimdiff
+git config credential.helper store
+
