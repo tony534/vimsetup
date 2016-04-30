2ca8c6540ae01b775e568f8855f1566b801bbac3
Wei Gao <gw8310@gmail.com>
2016-04-30 21:07:56 +1000

    fix errors

--- init.bat
+++ init.bat
@@ -1,10 +1,9 @@
-rm -rf ~\vimfiles
-rm -rf ~\_vimrc
-cp -R .\_vim ~\
-cp .\_vimrc ~\
+rm -rf %HOME%\vimfiles
+rm -rf %HOME%\_vimrc
+cp -R .\_vim %HOME%\
+cp .\_vimrc %HOME%\
 
-
-Powershell.exe -executionpolicy remotesigned -File  ~\vimsetup\ps.ps1
+Powershell.exe -executionpolicy remotesigned -File  %HOME%\vimsetup\ps.ps1
 
 git config --global user.name "Wei Gao"
 git config --global user.email "gw8310@gmail.com"
@@ -15,10 +14,9 @@ git config --global alias.st status
 git config --global push.default simple
 git config --global pull.rebase true
 git config --global rerere.enabled true
-git config --global alias.ss "status –s"
-git config --global alias.lg "log –oneline –decorate --all --graph"
-git config ­­--global alias.mylog "log ­­pretty=format:'%h %s [%an]' –graph"
+git config --global alias.ss "status -s"
+git config --global alias.lg "log -oneline -decorate --all --graph"
+git config --global alias.mylog "log --pretty=format:'%h %s [%an]' -graph"
 git config --global core.editor "mvim -f"
-git config --­­global merge.tool vimdiff
+git config --global merge.tool vimdiff
 git config credential.helper store
-
